c2618103a461d5fa1ada4071b066f042170cca21
elton-cs <govada.elton@gmail.com>
2026-04-24 19:12:07 -0500

    feat: cross board

--- src/resource.rs
+++ src/resource.rs
@@ -5,14 +5,16 @@ use bevy::{
 };
 
 pub fn cross_resource_plugin(app: &mut App) {
-    app.add_systems(Startup, (init_cross_resources, spawn_sprites).chain());
     app.add_systems(
-        Update,
-        debug_increment_frag_and_rune.run_if(input_pressed(KeyCode::Space)),
+        Startup,
+        (crosstile_init, fragrune_init, fragrune_spawn_sprites).chain(),
     );
     app.add_systems(
         Update,
-        toggle_frag_vis.run_if(input_just_pressed(KeyCode::Tab)),
+        (
+            fragrune_increment.run_if(input_pressed(KeyCode::Space)),
+            fragrune_toggle_vis.run_if(input_just_pressed(KeyCode::Tab)),
+        ),
     );
 }
 
@@ -28,7 +30,7 @@ struct Rune {
     amount: u32,
 }
 
-fn init_cross_resources(mut cmd: Commands) {
+fn fragrune_init(mut cmd: Commands) {
     for letter in 'A'..='Z' {
         let amount = 0;
         let frag = Fragment { letter, amount };
@@ -39,16 +41,16 @@ fn init_cross_resources(mut cmd: Commands) {
     }
 }
 
-fn spawn_sprites(
+fn fragrune_spawn_sprites(
     mut cmd: Commands,
     frag_q: Query<(Entity, &Fragment)>,
     rune_q: Query<(Entity, &Rune)>,
 ) {
-    let tile_size = 75.;
+    let tile_size = 50.;
     let tile_gap = tile_size / 20.;
     let size_multiplier = tile_size + tile_gap;
     let font_size = tile_size / 4.;
-    let y_offset = -3. * size_multiplier;
+    let y_offset = -5. * size_multiplier;
 
     let mut pos_vec = Vec::with_capacity(26);
     for (x, y) in (-6..=6).zip([0; 13]) {
@@ -63,7 +65,7 @@ fn spawn_sprites(
             Sprite::from_color(BLUE_700, Vec2::splat(tile_size)),
             Text2d::new(format!("{} {}", frag.letter, frag.amount)),
             TextFont::default().with_font_size(font_size),
-            Transform::from_xyz(x * size_multiplier, y * size_multiplier + y_offset, 2.),
+            Transform::from_xyz(x * size_multiplier, y * size_multiplier + y_offset, 3.),
             Visibility::Visible,
         ));
     }
@@ -73,13 +75,13 @@ fn spawn_sprites(
             Sprite::from_color(PURPLE_700, Vec2::splat(tile_size)),
             Text2d::new(format!("{} {}", rune.letter, rune.amount)),
             TextFont::default().with_font_size(font_size),
-            Transform::from_xyz(x * size_multiplier, y * size_multiplier + y_offset, 1.),
+            Transform::from_xyz(x * size_multiplier, y * size_multiplier + y_offset, 2.),
             Visibility::Visible,
         ));
     }
 }
 
-fn debug_increment_frag_and_rune(
+fn fragrune_increment(
     mut frag_q: Query<(&mut Fragment, &mut Text2d), Without<Rune>>,
     mut rune_q: Query<(&mut Rune, &mut Text2d)>,
 ) {
@@ -93,11 +95,51 @@ fn debug_increment_frag_and_rune(
     }
 }
 
-fn toggle_frag_vis(mut frag_q: Query<&mut Visibility, With<Fragment>>) {
+fn fragrune_toggle_vis(mut frag_q: Query<&mut Visibility, With<Fragment>>) {
     for mut vis in frag_q.iter_mut() {
         *vis = match *vis {
             Visibility::Visible => Visibility::Hidden,
             _ => Visibility::Visible,
         };
     }
+}
+
+#[derive(Component)]
+struct CrossTile {
+    letter: Option<char>,
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct CrossBoard;
+
+fn crosstile_init(mut cmd: Commands) {
+    let tile_size = 100.;
+    let tile_gap = tile_size / 20.;
+    let size_multiplier = tile_size + tile_gap;
+    let font_size = tile_size / 2.;
+
+    cmd.spawn((
+        CrossBoard,
+        Transform::from_xyz(0., size_multiplier / 2., 0.),
+        Sprite::default(),
+        Visibility::Visible,
+    ))
+    .with_children(|p| {
+        let range_x = 11;
+        let range_y = 5;
+        for x in -range_x / 2..=range_x / 2 {
+            for y in -range_y / 2..=range_y / 2 {
+                p.spawn((
+                    CrossTile { letter: None, x, y },
+                    Sprite::from_color(GRAY_900, Vec2::splat(tile_size)),
+                    Text2d::new(""),
+                    TextFont::default().with_font_size(font_size),
+                    Transform::from_xyz(x as f32 * size_multiplier, y as f32 * size_multiplier, 0.),
+                    Visibility::Inherited,
+                ));
+            }
+        }
+    });
 }
